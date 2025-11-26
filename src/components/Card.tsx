26987bf6afd01d399c662ba1d5d00a5307ca285d
hussein-zeaiter <husseinztr03@gmail.com>
2025-11-26 12:22:31 +0200

    initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+interface CardProps {
+    title: string;
+}
+
+function Card({ title }: CardProps) {
+
+    /* const x = 'Hello there dog\'';
+    const y = 'Hussein Zeaiter';
+    const z = `${x} ${y}`; */
+
+    return (
+        <div className={`card`}>
+            <h2>{title}</h2>
+        </div>
+    );
+}
+
+
+export default Card;
