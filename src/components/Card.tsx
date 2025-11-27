b5b9483717b97cf31b74a8794e19ad3da688aa70
hussein-zeaiter <husseinztr03@gmail.com>
2025-11-27 10:37:26 +0200

    changing readme and starting clean

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,17 @@
 interface CardProps {
-    title: string;
+  title: string;
 }
 
 function Card({ title }: CardProps) {
-
-    /* const x = 'Hello there dog\'';
+  /* const x = 'Hello there dog\'';
     const y = 'Hussein Zeaiter';
     const z = `${x} ${y}`; */
 
-    return (
-        <div className={`card`}>
-            <h2>{title}</h2>
-        </div>
-    );
+  return (
+    <div className={`card`}>
+      <h2>{title}</h2>
+    </div>
+  );
 }
-
 
 export default Card;
