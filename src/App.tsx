f1e897813138e2d9095a9f9c690c86984d6e41d2
hussein-zeaiter <husseinztr03@gmail.com>
2025-12-04 15:50:35 +0200

    resolved

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import thumb from "./assets/product/image-product-1-thumbnail.jpg";
 const dummyItems: Array<Item> = [
   {
     id: 1,
+    stockId: 1,
     img: thumb,
     title: "Fall Limited Edition Sneakers",
     qt: 2,
@@ -14,6 +15,7 @@ const dummyItems: Array<Item> = [
   },
   {
     id: 2,
+    stockId: 1,
     img: thumb,
     title: "Orange Summer Sandals",
     qt: 1,
